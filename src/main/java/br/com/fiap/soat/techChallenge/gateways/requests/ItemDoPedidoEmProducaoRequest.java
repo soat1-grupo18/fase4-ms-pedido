5cdfbcd87bfae23998513daf68c5cb2c8f30a852
Pablo Lima Dias <pabloidz@gmail.com>
2024-01-28 11:33:14 -0300

    Criar pedido em produção (ms-producao)

---
+++ src/main/java/br/com/fiap/soat/techChallenge/gateways/requests/ItemDoPedidoEmProducaoRequest.java
@@ -0,0 +1,25 @@
+package br.com.fiap.soat.techChallenge.gateways.requests;
+
+import br.com.fiap.soat.techChallenge.entities.ItemDoPedido;
+
+import java.math.BigDecimal;
+
+public class ItemDoPedidoEmProducaoRequest {
+    private String nome;
+    private String descricao;
+    private String categoria;
+    private String imagem;
+    private int quantidade;
+    private BigDecimal precoUnitario;
+
+    public static ItemDoPedidoEmProducaoRequest fromDomain(ItemDoPedido itemDoPedido) {
+        var itemDoPedidoEmProducaoRequest = new ItemDoPedidoEmProducaoRequest();
+        itemDoPedidoEmProducaoRequest.nome = itemDoPedido.getNome();
+        itemDoPedidoEmProducaoRequest.descricao = itemDoPedido.getDescricao();
+        itemDoPedidoEmProducaoRequest.categoria = itemDoPedido.getCategoria();
+        itemDoPedidoEmProducaoRequest.imagem = itemDoPedido.getImagem();
+        itemDoPedidoEmProducaoRequest.quantidade = itemDoPedido.getQuantidade();
+        itemDoPedidoEmProducaoRequest.precoUnitario = itemDoPedido.getPrecoUnitario();
+        return itemDoPedidoEmProducaoRequest;
+    }
+}
